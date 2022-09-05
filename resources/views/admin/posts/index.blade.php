195b2c5ee64f9f23b0dda775dcd29d08e70fefc1
jacoposergio <djago32@gmail.com>
2022-09-05 16:54:11 +0200

    posts index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.dashboard')
+
+@section('content')
+<h1>Lista dei posts</h1>
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,9 @@ Route::middleware('auth')
 ->name('admin.')
 ->prefix('admin')
 ->group(function(){
-Route::get('/', 'HomeController@index')->name('home');
-});
+        Route::get('/', 'HomeController@index')->name('home');
+        Route::resource('posts', 'PostController');
+}); 
 
 Route::get('{any?}', function () {
     return view('guest.home');
