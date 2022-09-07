1e251203d475f0e23f78d3e6723ca1699c662a54
jacoposergio <djago32@gmail.com>
2022-09-07 15:51:58 +0200

    foreign key

---
+++ database/migrations/2022_09_07_132106_add_category_id_to_post.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCategoryIdToPost extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // aggiungiamo la colonna category_id
+            $table->unsignedBigInteger('category_id')->nullable();
+            // aggiungere la relazione
+            $table->foreign('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // cancellare la relazione
+            $table->dropForeign('posts_category_id_foreign');
+            //cancellare la colonna
+            $table->dropColumn('category_id');
+        });
+    }
+}
