f4b8ccba948e9b1746144e995d4aa5982dff106e
jacoposergio <djago32@gmail.com>
2022-09-14 17:00:51 +0200

    done

--- resources/js/components/Posts.vue
+++ resources/js/components/Posts.vue
@@ -12,7 +12,7 @@
                         <div class="card-body">
                             <h5 class="card-title">{{post.title}}</h5>
                             <p class="card-text">{{ truncateText(post.content)}}</p>
-                            <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                             <router-link class="btn btn-primary" :to="{name : 'single-post', params: {slug: post.slug}}"/>Leggi</router-link>
                         </div>
                     </div>
             </div>
